dcc9927b8dee78e95df4a1d4d1d52a729196ce40
Laura <laura.colazzo@mail.polimi.it>
2022-05-28 18:41:45 +0200

    laura

---
+++ src/main/java/it/polimi/tiw/controllers/CheckLogin.java
@@ -0,0 +1,77 @@
+package it.polimi.tiw.controllers;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import it.polimi.tiw.DAO.UserDAO;
+import it.polimi.tiw.beans.User;
+import it.polimi.tiw.utils.ConnectionHandler;
+
+@WebServlet("/Login")
+public class CheckLogin extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+	private Connection connection = null;
+
+	public void init() throws ServletException{
+		ServletContext servletContext = getServletContext();
+		this.connection = ConnectionHandler.getConnection(servletContext);
+	}
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		UserDAO userDAO = new UserDAO(connection);
+		String username = request.getParameter("username");//escapeJava forse da fare
+		String password = request.getParameter("password");
+		User user;
+		
+		if(username == null || username.isEmpty() || password == null || password.isEmpty()) {
+			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			response.getWriter().println("Credentials must not be empty or null");
+			return;
+		}
+		
+		try {
+			user = userDAO.checkCredentials(username, password);
+		} catch (SQLException e) {
+			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			response.getWriter().println("Internal database error");			
+			return;
+		}
+		
+		if (user == null) {
+			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+			response.getWriter().println("Invalid credentials");
+			return;
+		} else {
+			request.getSession().setAttribute("user", user);
+			response.setStatus(HttpServletResponse.SC_OK);
+			response.setContentType("application/json");
+			response.setCharacterEncoding("UTF-8");
+			response.getWriter().println(username);
+			return;
+		}
+	}
+	
+	public void destroy() {
+		try {
+			ConnectionHandler.closeConnection(connection);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+
+}
